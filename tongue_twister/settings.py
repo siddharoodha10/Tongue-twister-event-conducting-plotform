a62396530734cfd5a5e2519302364c205a8d65d0
siddharoodha gubbewad <67213640+siddharoodha10@users.noreply.github.com>
2023-07-01 00:58:04 +0530

    Update settings.py

--- tongue_twister/settings.py
+++ tongue_twister/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4gaym%zp#!q6l1l#wg7f9^#lg&*8rq0bln%8+gw5&_)_hpoox#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aroodha.pythonanywhere.com']
 
 
 # Application definition
